d1fe5bb0090f90fb1a9f0478bc2d32eda3e9e1f6
quintessento <46756767+quintessento@users.noreply.github.com>
2020-06-04 19:10:40 +0200

    Initial commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿namespace AFSInterview
+{
+    using UnityEngine;
+
+    public class Bullet : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+
+        private GameObject targetObject;
+
+        public void Initialize(GameObject target)
+        {
+            targetObject = target;
+        }
+
+        private void Update()
+        {
+            var direction = (targetObject.transform.position - transform.position).normalized;
+
+            transform.position += direction * speed * Time.deltaTime;
+
+            if ((transform.position - targetObject.transform.position).magnitude <= 0.2f)
+            {
+                Destroy(gameObject);
+                Destroy(targetObject);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# unity-ancient-forge-tower-defence-interview
+ Interview task for a job at Ancient Forge Studio.

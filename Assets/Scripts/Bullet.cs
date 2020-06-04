7a2ea73eedfe0c05b3cca5c57cb4d2eb11e4ef7b
quintessento <46756767+quintessento@users.noreply.github.com>
2020-06-04 20:46:49 +0200

    Simple bullet fix
    - #2 fix. Enemy destruction by other towers no longer causes bullets to throw an exception.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,8 +5,11 @@
     public class Bullet : MonoBehaviour
     {
         [SerializeField] private float speed;
+        [SerializeField] private float autoDestroyDelay = 2f;
 
         private GameObject targetObject;
+
+        private Vector3 direction;
 
         public void Initialize(GameObject target)
         {
@@ -15,15 +18,30 @@
 
         private void Update()
         {
-            var direction = (targetObject.transform.position - transform.position).normalized;
+            if (targetObject != null)
+            {
+                direction = (targetObject.transform.position - transform.position).normalized;
+
+                if ((transform.position - targetObject.transform.position).magnitude <= 0.2f)
+                {
+                    Destroy(gameObject);
+                    Destroy(targetObject);
+                }
+            }
+            else
+            {
+                //enemy has already been destroyed by another tower, initiate self-destruction based on timer or checking when we go unseen under the ground
+                if(autoDestroyDelay > 0f && transform.position.y >= -0.1f)
+                {
+                    autoDestroyDelay -= Time.deltaTime;
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+            }
 
             transform.position += direction * speed * Time.deltaTime;
-
-            if ((transform.position - targetObject.transform.position).magnitude <= 0.2f)
-            {
-                Destroy(gameObject);
-                Destroy(targetObject);
-            }
         }
     }
 }
